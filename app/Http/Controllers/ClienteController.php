1c14f132255660d81ddeb9ecdf157dbc7b4a0b54
Luismiruua <114001610+Luismiruua@users.noreply.github.com>
2023-04-17 12:23:32 +0200

    'inicio-y-logout'

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -27,5 +27,42 @@ class ClienteController extends Controller
         ], 201);
     }
 
+    public function inicioSesion(Request $request){
+        //valida la entrada de datos al servidor
 
+        if($request->email){
+            $datos = $request->validate([
+                'email' => 'required|string|email',
+                'password' => 'required|string'
+            ]);
+        }else{
+            $datos = $request->validate([
+                'name' => 'required|string',
+                'password' => 'required|string'
+            ]);
+        }
+            if(!Auth::attempt($datos))
+                return response()->json([
+                    'message' => 'No estás autorizado, lo siento'],
+                401);
+
+
+        $user = $request->user();
+        $tokenResultado = $user->createToken('Personal acces Token');
+        $token = $tokenResultado->token;
+
+        return response()->json([
+            'info' =>  Auth::guard('api')->user(),
+            'access_token' => $tokenResultado->accessToken,
+        ]);
+    }
+
+
+    public function logout(){
+        Auth::guard('api')->user()->tokens()->delete();
+
+        return response()->json([
+            "message" => "Ha salido de su cuenta",
+        ]);
+    }
 }
